e0649db157483505d106c6089bdf1fe8ccb4f44e
lester zeor <lester@zeor.co>
2022-04-25 18:41:35 -0400

    added does not exist page

--- react-website/src/App.js
+++ react-website/src/App.js
@@ -1,15 +1,24 @@
 import Home from "./components/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 function App() {
   return (
     <div className="app_container">
       <Router>
         <Routes>
           <Route index path="/" element={<Home />} />
+          <Route path="/404" element={<ErrorPage />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
       </Router>
     </div>
   );
 }
-
+const ErrorPage = () => {
+  return <div>This page does not exist...</div>;
+};
 export default App;
