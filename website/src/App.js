7398e40584502c4bbec9680764efe8c7feab1aa0
lester zeor <lester@zeor.co>
2022-04-25 12:36:53 -0400

    cleaned up react app

---
+++ website/src/App.js
@@ -0,0 +1,9 @@
+import "./css/App.css";
+import getData from "./util/webdevtest-data.json";
+
+function App() {
+  console.log("getData", getData);
+  return <div className="App">application</div>;
+}
+
+export default App;
